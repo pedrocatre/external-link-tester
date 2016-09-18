c66f5d8a8f474e0416f901a1f22669361c699680
Pedro Catré <pedro.catre@payworksmobile.com>
2016-09-18 09:09:45 +0200

    removed commented out code

--- test.js
+++ test.js
@@ -32,16 +32,6 @@ test('my passing test', t => {
     }).testLinks();
 
     return externalLinksTester;
-    //const cp = childProcess.exec('./app.js ./extLinksTester.example.config.js', function() {
-    //
-    //});
-
-    //cp.on('error', t.ifError);
-    //
-    //cp.on('close', code => {
-    //    t.is(code, 0);
-    //    t.end();
-    //});
 });
 
 test('cli', async t => {
@@ -49,7 +39,6 @@ test('cli', async t => {
         t.truthy(result.stdout.includes('ERROR:  Link http://www.google.com111 is not ok. Error code was ENOTFOUND'));
         t.truthy(result.stdout.includes('SUCCESS:  Link https://en.wikipedia.org/wiki/List_of_HTTP_status_codes is ok.'));
         t.truthy(result.stdout.includes('All 2 tests finished running.'));
-        // TODO check file got created
     });
 });
 
